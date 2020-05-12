a2fa132dbbf4331714cf4cec6a74daace6ab4c8e
Your Name <you@example.com>
2020-05-12 01:51:05 -0500

    Cleanup

--- test.sh
+++ test.sh
@@ -20,17 +20,9 @@ EOF
   psql postgres <<EOF
 DROP DATABASE $SERVER_DB_NAME;
 EOF
-  psql postgres <<EOF
-DROP DATABASE cars_unit_test;
-EOF
 }
+
 trap "cleanup" QUIT TERM EXIT INT
-
-psql postgres <<EOF
-CREATE DATABASE cars_unit_test;
-EOF
-
-#ginkgo -r --cover
 
 psql postgres <<EOF
 CREATE DATABASE $SERVER_DB_NAME;
@@ -38,13 +30,11 @@ EOF
 psql $SERVER_DB_NAME -f cars-test.sql
 
 pushd server > /dev/null
-#go build -o $tmp_dir > /dev/null
 ./server &
 server_pid=$!
 popd > /dev/null
 
 pushd client > /dev/null
-#go build -o $tmp_dir > /dev/null
 ./client &
 client_pid=$!
 popd > /dev/null
